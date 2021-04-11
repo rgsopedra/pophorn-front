99564a89b417f3a92b73a61e0a922b713bc946fd
rgsopedra <rgsopedra@gmail.com>
2021-04-11 11:52:21 +0200

    Basic example app - working

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+
+function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctots Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Feb 6th at 1:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Feb 5th at 2:30pm',
+        reminder: false,
+    }
+  ])
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random()*10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks,newTask])
+  }
+  //Toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task ))
+  }
+
+  //Delete task
+  const deleteTask = (id) => {
+      setTasks(tasks.filter((task) => task.id !== id))
+  }
+  return (
+    <div className="container">
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask}/>}
+      {tasks.length===0 ? 'No Tasks to Show' : <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import Button from './Button'
+const Header = ({ title, onAdd, showAdd }) => {
+    return (
+        <header className='header'>
+            <h1> {title}</h1>
+            <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close': 'Add'} onClick={onAdd}/>
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: 'Default Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired
+}
+
+// const headintStyle = {
+//     color:'red', 
+//     backgroundColor:"black"
+// }
+
+export default Header
