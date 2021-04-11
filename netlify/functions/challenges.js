f54003bbd71753b2bb9a026461de358def0e220c
Daniel Escoz <darkhogg@gmail.com>
2021-04-11 18:39:28 +0200

    wip: add some fauna stuff

--- netlify/functions/challenges.js
+++ netlify/functions/challenges.js
@@ -1,10 +1,32 @@
+const faunadb = require('faunadb')
+
+const q = faunadb.query;
+
+
+var fauna = new faunadb.Client({
+    secret: process.env.FAUNADB_SECRET,
+    keepAlive: false,
+});
+
+
 exports.handler = async function(event, context) {
-    console.info("This is info of context" + context)
-    console.log("This is log event" + event)
-    console.warn("This is warn")
-    console.error("This is an error, yeah")
+    const result = await fauna.query(q.Map(
+        q.Paginate(q.Match(q.Index('all_challenges'))),
+        q.Lambda(ref => q.Get(ref))
+    ))
+
+    const challenges = result.data.map(obj => ({
+        $id: obj.ref.id,
+        ...obj.data,
+    }))
+
+    const body = {challenges};
+
     return {
         statusCode: 200,
-        body: JSON.stringify({message: "Hello World"})
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(body)
     };
 }
